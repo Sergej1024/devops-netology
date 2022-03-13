e08a57c174ad57f256a281a23a1dbbf437c871ef
Розум Сергей <sergej_r@list.ru>
2022-03-13 17:59:15 +0500

    update 4.2

--- 04-script-02-py/4.py
+++ 04-script-02-py/4.py
@@ -4,25 +4,26 @@ import socket as s
 import time as t
 import datetime as dt
 
-# set variables 
 i = 1
-wait=2 # интервал проверок в секундах
-srv = {'drive.google.com':'0.0.0.0', 'mail.google.com':'0.0.0.0', 'google.com':'0.0.0.0'}
+wait = 2  # интервал проверок в секундах
+srv = {'drive.google.com': '0.0.0.0', 'mail.google.com': '0.0.0.0', 'google.com': '0.0.0.0'}
 init = 0
 
-print('*** start script ***')
+print('*' * 37 + "start script" + '*' * 37)
 print(srv)
-print('********************')
+print('*'*86)
 
-while 1 == 1 : #отладочное число проверок
-  for host in srv:
-    ip = s.gethostbyname(host)
-    if ip != srv[host]:
-      if i == 1 and init != 1:
-        print(str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) +' [ERROR] ' + str(host) +' IP mistmatch: '+srv[host]+' '+ip)
-      srv[host] = ip
-# счетчик итераций для отладки, закомментировать для бесконечного цикла 3 строки
-  i += 1
-  if i >= 50 : 
-    break
-  t.sleep(wait)
+while 1 == 1:  # отладочное число проверок
+    for host in srv:
+        ip = s.gethostbyname(host)
+        if ip != srv[host]:
+            if i == 1 and init != 1:
+                print(
+                    str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + ' [ERROR] ' + str(host) + ' IP mistmatch: ' +
+                    srv[host] + ' ' + ip)
+            srv[host] = ip
+    # счетчик итераций для отладки, закомментировать для бесконечного цикла 3 строки
+#    i += 1
+#    if i >= 10:
+#        break
+    t.sleep(wait)
