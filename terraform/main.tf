3441e6125129e9c442892337f9813ded4eefb6ba
Розум Сергей <sergej_r@list.ru>
2022-06-09 13:12:42 +0500

    update 7.2 terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,49 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "yandex" {
-  token     = "YC_TOKEN"
-  cloud_id  = "YC_CLOUD_ID"
-  folder_id = "YC_FOLDER_ID"
-  zone      = "YC_ZONE"
+  token     = var.YC_TOKEN
+  cloud_id  = var.YC_CLOUD_ID
+  folder_id = var.YC_FOLDER_ID
+  zone      = var.YC_ZONE
 }
 
-resource "yandex_compute_image" "foo-image" {
-  name       = "my-custom-image"
-  source_url = "https://storage.yandexcloud.net/lucky-images/kube-it.img"
+resource "yandex_vpc_network" "network-1" {
+  name = "net"
 }
 
-resource "yandex_compute_instance" "vm" {
-  name = "vm-from-custom-image"
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  v4_cidr_blocks = ["10.1.0.0/24"]
+  zone           = var.YC_ZONE
+  network_id     = yandex_vpc_network.network-1.id
+}
 
-  # ...
+resource "yandex_compute_instance" "vm-1" {
+  name        = "linux-vm"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
 
   boot_disk {
     initialize_params {
-      image_id = "${yandex_compute_image.foo-image.id}"
+      image_id = "fd8aqitd4vl5950ihohp"
     }
   }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
